72c3e00ec92f03e5f655f6f6774d111f6e979db2
Hanna <anzhelenyuk@gmail.com>
2022-06-19 23:03:05 +0200

    Create a react native app

---
+++ App.js
@@ -0,0 +1,16 @@
+import NewPostScreen from './components/screens/NewPostScreen';
+import PostListScreen from './components/screens/PostListScreen';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{headerShown: false}} initialRouteName="Home">
+        <Stack.Screen name="PostList" component={PostListScreen} />
+        <Stack.Screen name="NewPost" component={NewPostScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
